ba36e04b9cf715a2557f0e567bc3b3fd21e7de43
Soichi Hayashi <hayashis@iu.edu>
2017-11-20 16:03:32 -0500

    added a few more UI parameters

--- preprocess.m
+++ preprocess.m
@@ -40,7 +40,6 @@ FUNCTIONAL_FILE=reshape(FUNCTIONAL_FILE,[nsubjects,nsessions]);
 STRUCTURAL_FILE={STRUCTURAL_FILE{1:nsubjects}};
 disp([num2str(size(FUNCTIONAL_FILE,1)),' subjects']);
 disp([num2str(size(FUNCTIONAL_FILE,2)),' sessions']);
-TR=2; % UI, Repetition time = 2 seconds
 
 %% CONN-SPECIFIC SECTION: RUNS PREPROCESSING/SETUP/DENOISING/ANALYSIS STEPS
 %% Prepares batch structure
@@ -52,7 +51,7 @@ batch.filename=fullfile(cwd,'output.mat');
 % CONN Setup.preprocessing                               (realignment/coregistration/segmentation/normalization/smoothing)
 batch.Setup.isnew=1;
 batch.Setup.nsubjects=1;
-batch.Setup.RT=TR;                                        % TR (seconds)
+batch.Setup.RT=config.tr;                               % UI TR (seconds)
 batch.Setup.functionals=repmat({{}},[1,1]);       % Point to functional volumes for each subject/session
 for nsub=1:1,
     for nses=1:nsessions,
@@ -71,7 +70,7 @@ else
     for ncond=1:nconditions,for nsub=1:1,for nses=ncond,        batch.Setup.conditions.onsets{1+ncond}{nsub}{nses}=0; batch.Setup.conditions.durations{1+ncond}{nsub}{nses}=inf;end;end;end % session-specific conditions
 end
 batch.Setup.preprocessing.steps=config.steps;
-batch.Setup.preprocessing.sliceorder=[2:2:34,1:2:33]; % UI, slice order acquisition
+batch.Setup.preprocessing.sliceorder=[2:2:config.sliceorder_a,1:2:config.sliceorder_b]; % UI, slice order acquisition
 batch.Setup.preprocessing.fwhm=config.fwhm; % UI, smoothing kernel
 batch.Setup.preprocessing.art_thresholds=[5,0.9]; % outlier detector, set to default
 batch.Setup.outputfiles=[0,1,0]; % writing of denoised data as nifti 
@@ -85,15 +84,14 @@ batch.Setup.overwrite='Yes';
 
 %% DENOISING step
 % CONN Denoising                                    % Default options (uses White Matter+CSF+realignment+scrubbing+conditions as confound regressors); see conn_batch for additional options 
-GS = 0; %UI, whether to perform fglobal signal regression or not
 for thispart = 1
-    if GS == 1
+    if config.do_global_signal_regression
         batch.Denoising.confounds.names={'Grey Matter','White Matter','CSF','realignment','scrubbing','Effect of rest'};
     else
         batch.Denoising.confounds.names={'White Matter','CSF','realignment','scrubbing','Effect of rest'};
     end
 end
-batch.Denoising.filter=[0.01, 0.1];                 % UI, frequency filter (band-pass values, in Hz)
+batch.Denoising.filter=[config.filter_bandpass_min, config.filter_bandpass_max];                 % UI, frequency filter (band-pass values, in Hz)
 batch.Denoising.done=1;
 batch.Denoising.overwrite='Yes';
 
